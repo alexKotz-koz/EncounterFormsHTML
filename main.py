ca1b9b5b59a740e1c6c3d506bd91de799d36aba6
Alex Kotz <alexKotz@github.com>
2021-11-16 08:52:16 -0700

    Layout/Skeleton done for Home, CPOE A&P, find Patient.
    Refactored to maintain pid through site. Added logic functions. CPOE A&P layout done. Pre-PatientAssessments logic.

--- main.py
+++ main.py
@@ -31,6 +31,16 @@ class PatientProblems(db.Model):
     PID = db.Column(db.Integer, db.ForeignKey('patients.id'), autoincrement=True)
 
 
+class PatientAssessments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    DxName = db.Column(db.String(25))
+    Assessment1 = db.Column(db.String(500))
+    Assessment1Date = db.Column(db.String(10))
+    Assessment2 = db.Column(db.String(500))
+    Assessment2Date = db.Column(db.String(10))
+    PID = db.Column(db.Integer, db.ForeignKey('patients.id'))
+
+
 class GlobalProblemList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     DxName = db.Column(db.String(25))
@@ -45,32 +55,32 @@ def addGlobalProblem(DxName, DxCode, DxDescription):
     db.session.add(GlobalProblemList(DxName=DxName, DxCode=DxCode, DxDescription=DxDescription))
     db.session.commit()
 
-def getID (patientFirst):
+
+def getID(patientFirst):
     patient = Patients.query.filter_by(First=patientFirst).first()
     return patient
 
 
-@app.route('/', methods=['GET','POST'])
-def home():
-    db.create_all()
-    patient = Patients.query.all()
-
-    return render_template('home.html', patient=patient)
-
-@app.route('/DropTables',methods=['GET','POST'])
+# Begin Logic Routes
+@app.route('/DropTables', methods=['GET', 'POST'])
 def DropTables():
     if request.method == 'POST':
         db.drop_all()
-        return redirect('/')
-@app.route('/DropProblems', methods=['GET','POST'])
+        return redirect('/home')
+
+
+@app.route('/DropProblems', methods=['GET', 'POST'])
 def DropProblems():
     patients = PatientProblems.query.all()
     if request.method == 'POST':
         for pt in patients:
             db.session.delete(pt)
             db.session.commit()
-        return redirect('/')
+        return redirect('/home')
 
+
+# End Logic Routes
+# Begin Pop Up Routes
 @app.route('/AddPt', methods=['GET', 'POST'])
 def AddPt():
     if request.method == 'POST':
@@ -85,67 +95,113 @@ def AddPt():
                                 PrimaryProvider=primaryProvider, Phone=phone))
         db.session.commit()
         return redirect('/')
-    return render_template('AddPt.html')
+    return render_template('addPt.html')
 
 
-@app.route('/AddProblem/<id>', methods=['POST', 'GET'])
-def AddProblem(id):
-    Pid = Patients.query.filter_by(id=id).first()
+@app.route('/AddProblem/<pid>', methods=['POST', 'GET'])
+def AddProblem(pid):
+    # Pid = Patients.query.filter_by(id=id).first()
+
     globalProblemList = GlobalProblemList.query.all()
-    patients = Patients.query.all()
+    patients = Patients.query.filter_by(id=pid)
     if request.method == 'POST':
         onSetDate = request.form['onSetDate']
         patient = request.form['findPatient']
-        parsePatient = str(patient).split(' ')
-        patiedPID = int(parsePatient[2])
-
-
 
         if patient is not None:
             dxName = request.form['selectProblem']
             globalProblemListDxName = GlobalProblemList.query.filter_by(DxName=dxName).first()
 
             if globalProblemListDxName.DxName == dxName:
-                db.session.add(PatientProblems(DxName=globalProblemListDxName.DxName,
+                problemToAdd = PatientProblems(DxName=globalProblemListDxName.DxName,
                                                DxCode=str(GlobalProblemList.DxCode),
                                                DxDescription=str(GlobalProblemList.DxDescription),
                                                DxOnSetDate=onSetDate,
-                                               PID=Pid))
+                                               PID=pid)
+                db.session.add(problemToAdd)
                 db.session.commit()
+                return redirect('/CPOE/' + pid)
             else:
-                return redirect('/AddProblem')
-    return render_template('AddProblem.html', globalProblemList=globalProblemList, patients=patients)
+                return redirect('/AddProblem/' + pid)
+    return render_template('addProblem.html', globalProblemList=globalProblemList, patients=patients, pid=pid)
 
 
-@app.route('/AddProblemGlobal', methods=['POST', 'GET'])
+@app.route('/AddProblemGlobal', methods=['GET', 'POST'])
 def AddProblemToGlobalList():
     if request.method == 'POST':
         DxName = request.form['DxName']
         DxCode = request.form['DxCode']
         DxDescription = request.form['DxDescription']
         addGlobalProblem(DxName, DxCode, DxDescription)
-        return redirect('/CPOE')
-    return render_template('AddProblemGlobal.html', GlobalProblemList=GlobalProblemList)
+        return redirect('/')
+    return render_template('addProblemGlobal.html', GlobalProblemList=GlobalProblemList)
+
+
+# End Pop Up Routes
+
+# Begin Page Routes
+@app.route('/', methods=['GET', 'POST'])
+def init():
+    db.create_all()
+    patient = Patients.query.all()
+
+    return render_template('init.html', patient=patient)
+
+
+@app.route('/findPatient', methods=['GET', 'POST'])
+def findPatient():
+    if request.method == 'POST':
+        searchForRadio = request.form['searchForPatient']
+        if searchForRadio == "pid":
+            pid = request.form['searchFor']
+            patientPID = Patients.query.filter_by(id=pid).first()
+            return redirect('/home/' + str(patientPID.id))
+        elif searchForRadio == "name":
+            name = request.form['searchFor']
+            patientName = Patients.query.filter_by(First=name).first()
+            pid = patientName.id
+            return redirect('/home/' + str(pid))
+        else:
+            flash("Must select search by type")
+
+    return render_template('findPatient.html')
+
+
+@app.route('/home/<pid>', methods=['GET', 'POST'])
+def home(pid):
+    patient = Patients.query.filter_by(id=pid)
+
+    return render_template('home.html', patient=patient)
+
 
 @app.route('/ChartSummary', methods=['POST', 'GET'])
 def ChartSummary():
     patients = Patients.query.all()
     patientProblems = PatientProblems.query.all()
-    return render_template('ChartSummary.html', patients=patients, patientProblems=patientProblems)
+    return render_template('chartSummary.html', patients=patients, patientProblems=patientProblems)
 
 
-@app.route('/CPOE', methods=['POST', 'GET'])
-def CPOE():
-    patients = Patients.query.all()
-    patientProblems = PatientProblems.query.all()
-
+@app.route('/CPOE/<pid>', methods=['POST', 'GET'])
+def CPOE(pid):
+    patient = Patients.query.filter_by(id=pid)
+    patientProblems = PatientProblems.query.filter_by(PID=pid)
+    patientAssessments = PatientAssessments.query.filter_by(PID=pid).first()
+    today = datetime.date.today()
     if request.method == 'POST':
-        patientSelected = request.form['selectPatient']
+        if request.form.get('commitA&P1'):
+            assessment1 = request.form['assessment1']
+            assessment1Add = PatientAssessments(Assessment1=assessment1, Assessment1Date=today)
+            return redirect('/home/' + str(pid))
 
-        print(patientSelected)
+        elif request.form.get('commitA&P2'):
+            assessment2 = request.form['assessment2']
+            return redirect('/home/' + str(pid))
+    return render_template('CPOE.html', patient=patient, patientProblems=patientProblems, pid=pid)
 
 
-    return render_template('CPOE.html', patients=patients, patientProblems=patientProblems)
+@app.route('/CarePlan')
+def CarePlan():
+    return render_template('carePlan.html')
 
 
 @app.route('/HPI')
@@ -155,7 +211,7 @@ def HPI():
 
 @app.route('/PtInstructions')
 def PtInstructions():
-    return render_template('PtInstructions.html')
+    return render_template('ptInstructions.html')
 
 
 @app.route('/VS')
